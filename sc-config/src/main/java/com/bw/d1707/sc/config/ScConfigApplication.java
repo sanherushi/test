f357a02fa1fcf52d2bb3ab1839214ac30c21c732
sanherushi <2113739641@qq.com>
2020-03-27 17:32:09 +0800

    springcloud学习3.27 添加configfiles文件夹

---
+++ sc-config/src/main/java/com/bw/d1707/sc/config/ScConfigApplication.java
@@ -0,0 +1,13 @@
+package com.bw.d1707.sc.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ScConfigApplication {
+    public static void main(String[] args) {
+            SpringApplication.run(ScConfigApplication.class,args);
+        }
+}
